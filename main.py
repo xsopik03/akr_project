2f40ba734d3838d66a7a6efefef679155c9a3af2
Vlachovi <pepi.vlach@gmail.com>
2020-11-09 15:16:42 +0100

    Vytvoření console.

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 #tak to tady musímě pěkně napsat aby jsme měli hodně bodíků
 #JV úspěšně commitnul
+
 #Funkce zajišťuje načtení posílané zprávy uživatelem 1
 def nacteni_zpravy():
     print("Zadej zprávu, která má být přenesena:")
     uzivatel = input()
+    konsole("Odesilatel: " + uzivatel)
     return uzivatel
 
 #Funkce bude zajišťovat šifrový text.
-def sifrovy_text(zprava):
+def sifrovy_text(zprava, klic):
     #Později bude volat šifrovací funkci a jako parametr bude předávat načtený text.
     sifrovy_text = zprava
+    konsole("Šifrovy text: " + sifrovy_text)
     return sifrovy_text
 
 #Funkce zajišťuje rozšifrován posílané zprávy uživatelem 1
-def rozsifrovani_sifroveho_textu(sifrovy_text):
+def rozsifrovani_sifroveho_textu(sifrovy_text,klic):
     # Později bude volat šifrovací funkci a jako parametr bude předávat načtený text.
     uzivatel2 = sifrovy_text
+    konsole("Přijemce: " + uzivatel2)
     return uzivatel2
 
 #Funkce vygeneruje symetricky klíč a uloží ho do globální proměnné.
 def AES_generovani_klicu():
-    print("TODO generovani klíče.")
+    klic = "TODO klic"
+    konsole("Posílám klíč: " + klic)
     #TODO generovaní symetrického klíče!
+    return klic
 
 #Funkce zašifruje text pomoci symetrické šifry AES.
 def AES_sifrovani(key, text):
-    print("TODO")
     #TODO šifrovani!
     s_text = text
     return s_text
@@ -37,17 +42,17 @@ def AES_desifrovani(key, s_text):
     text = s_text
     return text
 
+#Funkce, které budeme předávat texty, které se mají vypsat do konsole. Text, který budeme chtít zobrazit předáme funkci konsole.
+def konsole(konsol_text):
+    print("Konsole: " + konsol_text)
+
+
 #Komunikacni kanál zobrazuje probíhající komunikaci mezi uživately a přenášený šifrový text.
 def komunikacni_kanal():
     zprava = nacteni_zpravy()
-    s_text = sifrovy_text(zprava)
-    vystup = rozsifrovani_sifroveho_textu(s_text)
-    print("Odesilatel -> Uživatel2: " + zprava)
-    print("Šifrový text: " + s_text)
-    print("Uživatel 1 -> Příjemce: " + vystup)
+    klic = AES_generovani_klicu()
+    s_text = sifrovy_text(zprava, klic)
+    rozsifrovani_sifroveho_textu(s_text, klic)
 
-print("Pokus!")
+#main
 komunikacni_kanal()
-
-
-
