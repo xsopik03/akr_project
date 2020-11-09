f70ee97bdc882f8903e8cc53fa465e20e3993b8b
Vlachovi <pepi.vlach@gmail.com>
2020-11-09 19:17:05 +0100

    Příprava na šifrování pomoci RSA.

--- main.py
+++ main.py
@@ -1,46 +1,83 @@
 #tak to tady musímě pěkně napsat aby jsme měli hodně bodíků
 #JV úspěšně commitnul
 
+p_symetricky_klic = "";
+r_symetricky_klic = "";
+s_symetricky_klic = "";
+asymetricky_klic1 = "";
+asymetricky_klic2 = "";
+p_zprava = "";
+r_zprava = "";
+s_zprava = "";
+
+#Switch class, vytvoření vlastního switch.
+class switch(object):
+    value = None
+    def __new__(class_, value):
+        class_.value = value
+        return True
+#Funkce spolupracující s switch.
+def case(*args):
+    return any((arg == switch.value for arg in args))
+
 #Funkce zajišťuje načtení posílané zprávy uživatelem 1
 def nacteni_zpravy():
-    print("Zadej zprávu, která má být přenesena:")
+    konsole("Zadej zprávu, která má být přenesena:")
     uzivatel = input()
     konsole("Odesilatel: " + uzivatel)
-    return uzivatel
+    global p_zprava
+    p_zprava = uzivatel
 
 #Funkce bude zajišťovat šifrový text.
-def sifrovy_text(zprava, klic):
+def sifrovy_text():
     #Později bude volat šifrovací funkci a jako parametr bude předávat načtený text.
-    sifrovy_text = zprava
+    sifrovy_text = s_zprava
     konsole("Šifrovy text: " + sifrovy_text)
-    return sifrovy_text
 
 #Funkce zajišťuje rozšifrován posílané zprávy uživatelem 1
-def rozsifrovani_sifroveho_textu(sifrovy_text,klic):
+def rozsifrovani_sifroveho_textu():
     # Později bude volat šifrovací funkci a jako parametr bude předávat načtený text.
-    uzivatel2 = sifrovy_text
-    konsole("Přijemce: " + uzivatel2)
-    return uzivatel2
+    zprava = r_zprava
+    konsole("Přijemce: " + zprava)
 
 #Funkce vygeneruje symetricky klíč a uloží ho do globální proměnné.
-def AES_generovani_klicu():
-    klic = "TODO symetrický klic"
-    konsole("Posílám symetrický klíč: " + klic)
-    #TODO generovaní symetrického klíče!
-    return klic
+def AES_generovani_klicu(key):
+    # TODO generovaní symetrického klíče!
+    global p_symetricky_klic, r_symetricky_klic
+    if key == "p":p_symetricky_klic = "TODO generování symetrického klíče"
+    if key == 'r':r_symetricky_klic = "TODO generování druhého klíče"
+    konsole("Generuji symetrick7 klíč:" + p_symetricky_klic)
 
 #Funkce zašifruje text pomoci symetrické šifry AES.
-def AES_sifrovani(key, text):
+def AES_sifrovani():
     #TODO šifrovani!
-    s_text = text
-    return s_text
+    global s_zprava, rsa
+    if p_symetricky_klic == "": AES_generovani_klicu("p"), RSA_sifrovani()
+    s_zprava = "TODO sifrovani zpravy"
 
 #Funkce dešifruje text pomoci symetrické šifry AES.
-def AES_desifrovani(key, s_text):
-    print("TODO")
+def AES_desifrovani():
     #TODO dešifrovani!
-    text = s_text
-    return text
+    if r_symetricky_klic == "": AES_generovani_klicu("r"), RSA_desifrovani()
+    #Teoreticky není potřeba, zprávu pořád máme, takže stačí vypsat zprávu, ale pro úplnost, bych dešifroval
+    global r_zprava
+    r_zprava = p_zprava
+
+#Funkce na šifrování symetrického klíče.
+def RSA_sifrovani():
+    #TODO rsa sidrovani.
+    konsole("Šifrování veřejného klíče. TODO")
+    global s_symetricky_klic
+    s_symetricky_klic = "TODO šifrování ss klíče pomoci RSA"
+    konsole("Posílám šifrovaný klíč, TODO " + s_symetricky_klic)
+
+#Funkce na dešifrování symetrického klíče.
+def RSA_desifrovani():
+    #TODO RSA desifrovani.
+    konsole("Dešifrování veřejného klíče. TODO")
+    global r_symetricky_klic
+    r_symetricky_klic = "TODO dešifrovani ss klíče pomoci RSA"
+    konsole("Dešifruji klíč, TODO " + r_symetricky_klic)
 
 #Funkce, které budeme předávat texty, které se mají vypsat do konsole. Text, který budeme chtít zobrazit předáme funkci konsole.
 def konsole(konsol_text):
@@ -49,10 +86,36 @@ def konsole(konsol_text):
 
 #Komunikacni kanál zobrazuje probíhající komunikaci mezi uživately a přenášený šifrový text.
 def komunikacni_kanal():
-    zprava = nacteni_zpravy()
-    klic = AES_generovani_klicu()
-    s_text = sifrovy_text(zprava, klic)
-    rozsifrovani_sifroveho_textu(s_text, klic)
+    nacteni_zpravy()
+    AES_sifrovani()
+    sifrovy_text()
+    AES_desifrovani()
+    rozsifrovani_sifroveho_textu()
+
+#Menu applikace, možné po zadání čísla vyzvat program k dané operaci.
+def menu():
+    print("Menu:\n1. Zahájení konverzace.\n2. Vygenerování nových symetrických klíčů.\n3. TODO něco.\n4. Konec")
+    chose = input()
+    while switch(chose):
+        if case("1"):
+            konsole("Zahájení konvezace.")
+            komunikacni_kanal()
+            return 1
+        if case("2"):
+            konsole("Generování nových AES klíčů.")
+            AES_generovani_klicu(o)
+            return 1
+        if case("3"):
+            konsole("TODO něco")
+            return 1
+
+        if case("4"):
+            konsole("Konec")
+            return 0
+        print("Špatně zvoleno.")
+        break
 
 #main
-komunikacni_kanal()
+end = 1
+while end == 1:
+    end = menu()
