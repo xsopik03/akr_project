7c5376492ca8de7cfa4200d8252f1de8e8a54730
jakubskoda <jakub471@gmail.com>
2020-12-01 19:15:25 +0100

    RSA šifrování AES klíče

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
-#tak to tady musímě pěkně napsat aby jsme měli hodně bodíků
-#JV úspěšně commitnul
-#zkouška
+# tak to tady musímě pěkně napsat aby jsme měli hodně bodíků
 
 import os
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 from cryptography.hazmat.primitives.ciphers import (
     Cipher, algorithms, modes
 )
-#TODO udělat kládání konverzace + klíče do souboru a jejich načtení
-#Původní symetrický klíč
+# TODO udělat kládání konverzace + klíče do souboru a jejich načtení
+# Původní symetrický klíč
 p_symetricky_klic = ""
-#Rozšifrovaný symetrický klíč
+# Rozšifrovaný symetrický klíč
 r_symetricky_klic = ""
-#Šifrovany klič pomoci RSA
+# Šifrovany klič pomoci RSA
 s_symetricky_klic = ""
-#RSA klíč 1
-asymetricky_klic1 = ""
-#RSA klíč 2
-asymetricky_klic2 = ""
-#Půvdoní zpráva
+# public RSA key
+public_rsa_key = ""
+# private RSA key
+private_rsa_key = ""
+# Půvdoní zpráva
 p_zprava = ""
-#Rozšifrovaná zpráva
+# Rozšifrovaná zpráva
 r_zprava = ""
-#Sifrovaná zpráva
+# Sifrovaná zpráva
 s_zprava = ""
-#Random proměnná pro generování kníče
+# Random proměnná pro generování kníče
 random = ""
-#bytová verze zprávy
+# bytová verze zprávy
 b_zprava = ""
-#Kontrola, zda byla zahájena konverzace
+# Kontrola, zda byla zahájena konverzace
 zapocata_komunikace = 0
-#pomocna p
+# pomocna p
 p = ""
 # globální iv = z klíče - předat returnem z šifrování do dešifrování AES
 global_iv = ""
 
-#Switch class, vytvoření vlastního switch.
+# Switch class, vytvoření vlastního switch.
 class switch(object):
     value = None
     def __new__(class_, value):
@@ -51,16 +50,16 @@ def case(*args):
 
 #Funkce zajišťuje načtení posílané zprávy uživatelem 1
 def nacteni_zpravy():
-    konsole("Zadej zprávu, která má být přenesena:")
+    print("Zadej zprávu, která má být přenesena:")
     uzivatel = input()
-    konsole("Uživatel1: " + uzivatel)
+    print("Uživatel1: " + uzivatel)
     global p_zprava
     p_zprava = uzivatel
 
 def nacteni_zpravy_odpoved():
-    konsole("Zadej zprávu, která má být přenesena:")
+    print("Zadej zprávu, která má být přenesena:")
     uzivatel = input()
-    konsole("Uživatel2: " + uzivatel)
+    print("Uživatel2: " + uzivatel)
     global p_zprava
     p_zprava = uzivatel
 
@@ -71,20 +70,20 @@ def sifrovy_text():
     #sifrovy_text = Decode(sifrovy_text)
     #s_zprava = Decode(s_zprava)
     #print("sifrovy text")
-    konsole("Šifrový text: " + str(sifrovy_text))
+    print("Šifrový text: " + str(sifrovy_text))
 
 #Funkce zajišťuje rozšifrován posílané zprávy uživatelem 1
 def rozsifrovani_sifroveho_textu():
     # Později bude volat šifrovací funkci a jako parametr bude předávat načtený text.
     zprava = r_zprava
     zprava = Decode(zprava)
-    konsole("Přijemce: " + zprava)
+    print("Přijemce: " + zprava)
 
 #Funkce vygeneruje symetricky klíč a uloží ho do globální proměnné.
 def AES_generovani_klicu(key_G):
     # Slouží pro předání a uložení generovaného klíče
     global random,p_symetricky_klic, r_symetricky_klic
-    #Generuje nový random klíč
+    # Generuje nový random klíč
     passw = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -94,16 +93,19 @@ def AES_generovani_klicu(key_G):
         backend=default_backend()
     )
     key = base64.urlsafe_b64encode(kdf.derive(passw))
-    #print(len(key))
-    #key = os.urandom(16)
-    #Zajišťuje uložení klíče do globální proměné
+    # print(len(key))
+    # key = os.urandom(16)
+    # Zajišťuje uložení klíče do globální proměné
     if (random == ""): random = key
 
-    #Zajišťuje generování dle parametru, záleží na tom - jaké je potřeba generovat
-    #TODO pepík předělání
-    if key_G == "p":konsole("Generuji symetrický klíč.");p_symetricky_klic = key;print("Konsole: Generovaný klíč:");print(Decode(p_symetricky_klic));konsole("Posílám vygenerované klíče");
-    if key_G == 'r':r_symetricky_klic = random;print("Konsole: Předaný klíč:");print(Decode(r_symetricky_klic))
-    if key_G == 'o':konsole("Generuji symetrický klíč.");p_symetricky_klic = key; r_symetricky_klic = key;print("Konsole: Generovaný klíč:");print(Decode(r_symetricky_klic))
+    # Zajišťuje generování dle parametru, záleží na tom - jaké je potřeba generovat
+    # TODO pepík předělání
+    if key_G == "p":print("Generuji symetrický klíč.");p_symetricky_klic = key;print("print: Generovaný klíč:");print(Decode(p_symetricky_klic));print("Posílám vygenerované klíče");
+    if key_G == 'r':r_symetricky_klic = random;print("print: Předaný klíč:");print(Decode(r_symetricky_klic))
+    if key_G == 'o':print("Geneuji symetrický klíč.");p_symetricky_klic = key; r_symetricky_klic = key;print("print: Generovaný klíč:");print(Decode(r_symetricky_klic))
+
+    # Šifrování AES klice
+    RSA_sifrovani()
 
 #Funkce zašifruje text pomoci symetrické šifry AES.
 def AES_sifrovani():
@@ -143,13 +145,14 @@ def Decode(zprava):
 
 #Funkce dešifruje text pomoci symetrické šifry AES.
 def AES_desifrovani():
+    RSA_desifrovani()
     if r_symetricky_klic == "": AES_generovani_klicu("r")
-    #Teoreticky není potřeba, zprávu pořád máme, takže stačí vypsat zprávu, ale pro úplnost, bych dešifroval
+    # Teoreticky není potřeba, zprávu pořád máme, takže stačí vypsat zprávu, ale pro úplnost, bych dešifroval
     global r_zprava
-    #Tohle nahradit dešiforováním
-    #f = Fernet(p_symetricky_klic)
-    #r_zprava = f.decrypt(s_zprava)
-    #r_zprava = Decode(r_zprava)
+    # Tohle nahradit dešiforováním
+    # f = Fernet(p_symetricky_klic)
+    # r_zprava = f.decrypt(s_zprava)
+    # r_zprava = Decode(r_zprava)
     decryptor = Cipher(
         algorithms.AES(p_symetricky_klic),
         modes.GCM(global_iv, p),
@@ -159,27 +162,47 @@ def AES_desifrovani():
     decryptor.authenticate_additional_data(b"authenticated but not encrweweweweypted payload")
 
     r_zprava = decryptor.update(s_zprava) + decryptor.finalize()
-    #Po sem
 
-#Funkce na šifrování symetrického klíče.
+# Funkce na šifrování symetrického klíče.
 def RSA_sifrovani():
-    #TODO rsa sidrovani.
-    konsole("Šifrování veřejného klíče. TODO")
+    print("Šifrování AES klíče " + str(p_symetricky_klic))
     global s_symetricky_klic
-    s_symetricky_klic = "TODO šifrování ss klíče pomoci RSA"
-    konsole("Posílám šifrovaný klíč, TODO " + s_symetricky_klic)
+    global private_rsa_key
+    private_rsa_key = rsa.generate_private_key(public_exponent=65537,key_size=2048, backend=default_backend())
+    print("Privátní RSA klíč " +  str(private_rsa_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    )))
+    global public_rsa_key
+    public_rsa_key = private_rsa_key.public_key()
+    print("Veřejný RSA klíč " + str(public_rsa_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )))
 
-#Funkce na dešifrování symetrického klíče.
+    s_symetricky_klic = public_rsa_key.encrypt(p_symetricky_klic,
+        padding=padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    print("Posílám šifrovaný AES klíč, " + str(s_symetricky_klic))
+
 def RSA_desifrovani():
-    #TODO RSA desifrovani.
-    konsole("Dešifrování veřejného klíče. TODO")
+    print("Dešifrování AES klíče")
     global r_symetricky_klic
-    r_symetricky_klic = "TODO dešifrovani ss klíče pomoci RSA"
-    konsole("Dešifruji klíč, TODO " + r_symetricky_klic)
+    r_symetricky_klic = private_rsa_key.decrypt(s_symetricky_klic,
+        padding=padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
 
-#Funkce, které budeme předávat texty, které se mají vypsat do konsole. Text, který budeme chtít zobrazit předáme funkci konsole.
-def konsole(konsol_text):
-    print("Konsole: " + konsol_text)
+    print("Dešifrovaný AES klíč " + str(r_symetricky_klic))
 
 #Komunikacni kanál zobrazuje probíhající komunikaci mezi uživately a přenášený šifrový text.
 def komunikacni_kanal():
@@ -210,10 +233,10 @@ def komunikacni_kanal_pokracovani():
             if case("2"):
                 komunikacni_kanal_odpoved()
             if case("3"):
-                konsole("Poslední posílaná zpráva.")
+                print("Poslední posílaná zpráva.")
                 rozsifrovani_sifroveho_textu()
             if case("4"):
-                konsole("Vracíme se do hlavního menu.")
+                print("Vracíme se do hlavního menu.")
                 end = 0
             #if(chose != 1 && chose != 2 && chose != 3 && chose != 4): print("Špatně zvoleno.")
             break
@@ -225,26 +248,27 @@ def menu():
     chose = input()
     while switch(chose):
         if case("1"):
-            konsole("Zahájení konvezace.")
+            print("Zahájení konvezace.")
             komunikacni_kanal()
             return 1
         if case("2"):
-            konsole("Pokračování v započaté konverzaci.")
+            print("Pokračování v započaté konverzaci.")
             if zapocata_komunikace == 1: komunikacni_kanal_pokracovani()
-            else: konsole("Žádná konverzace nebyla započatá.")
+            else: print("Žádná konverzace nebyla započatá.")
             return 1
         if case("3"):
-            konsole("Generování nových AES klíčů.")
+            print("Generování nových AES klíčů.")
             AES_generovani_klicu("o")
             return 1
 
         if case("4"):
-            konsole("Konec")
+            print("Konec")
             return 0
         print("Špatně zvoleno.")
         break
 
-#main
+# main
 end = 1
 while end == 1:
     end = menu()
+
